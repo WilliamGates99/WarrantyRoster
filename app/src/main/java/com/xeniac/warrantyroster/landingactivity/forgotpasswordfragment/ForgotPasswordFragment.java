648cdc1d2612a5c03a67839756073bebc655b2ed
WilliamGates99 <william.gates.3299@gmail.com>
2021-08-25 22:13:58 +0430

    Add Forgot Password Fragment

--- app/src/main/java/com/xeniac/warrantyroster/landingactivity/forgotpasswordfragment/ForgotPasswordFragment.java
+++ app/src/main/java/com/xeniac/warrantyroster/landingactivity/forgotpasswordfragment/ForgotPasswordFragment.java
@@ -2,8 +2,6 @@ package com.xeniac.warrantyroster.landingactivity.forgotpasswordfragment;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,10 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import com.xeniac.warrantyroster.Constants;
 import com.xeniac.warrantyroster.R;
 import com.xeniac.warrantyroster.databinding.FragmentForgotPasswordBinding;
-import com.xeniac.warrantyroster.mainactivity.MainActivity;
 
 public class ForgotPasswordFragment extends Fragment {
 
