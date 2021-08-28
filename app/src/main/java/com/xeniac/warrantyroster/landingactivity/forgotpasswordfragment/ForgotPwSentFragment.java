d3b23ef44f109f49c88b54f7bb4ee9a143a8142f
WilliamGates99 <william.gates.3299@gmail.com>
2021-08-28 21:49:00 +0430

    Add Forgot Password Send Fragment

--- app/src/main/java/com/xeniac/warrantyroster/landingactivity/forgotpasswordfragment/ForgotPwSentFragment.java
+++ app/src/main/java/com/xeniac/warrantyroster/landingactivity/forgotpasswordfragment/ForgotPwSentFragment.java
@@ -3,6 +3,8 @@ package com.xeniac.warrantyroster.landingactivity.forgotpasswordfragment;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,10 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.xeniac.warrantyroster.R;
 import com.xeniac.warrantyroster.databinding.FragmentForgotPwSentBinding;
+
+import java.text.DecimalFormat;
 
 public class ForgotPwSentFragment extends Fragment {
 
@@ -21,6 +26,8 @@ public class ForgotPwSentFragment extends Fragment {
     private Activity activity;
     private Context context;
     private NavController navController;
+
+    private CountDownTimer countDownTimer;
 
     public ForgotPwSentFragment() {
     }
@@ -35,6 +42,7 @@ public class ForgotPwSentFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        countDownTimer.cancel();
         forgotPwSentBinding = null;
     }
 
@@ -44,5 +52,51 @@ public class ForgotPwSentFragment extends Fragment {
         activity = getActivity();
         context = getContext();
         navController = Navigation.findNavController(view);
+
+        resendOnClick();
+        returnOnClick();
+    }
+
+    private void resendOnClick() {
+        forgotPwSentBinding.btnForgotPwSentResend.setOnClickListener(view -> {
+            countdown();
+            forgotPwSentBinding.lavForgotPwSent.playAnimation();
+        });
+    }
+
+    private void returnOnClick() {
+        forgotPwSentBinding.btnForgotPwSentReturn.setOnClickListener(view ->
+                activity.onBackPressed());
+    }
+
+    private void countdown() {
+        forgotPwSentBinding.tvForgotPwSentResend.setVisibility(View.GONE);
+        forgotPwSentBinding.btnForgotPwSentResend.setVisibility(View.GONE);
+        forgotPwSentBinding.tvForgotPwSentResent.setVisibility(View.VISIBLE);
+        forgotPwSentBinding.tvForgotPwSentTimer.setVisibility(View.VISIBLE);
+
+        DecimalFormat decimalFormat = new DecimalFormat("00");
+        int startTime = 120 * 1000;
+
+        countDownTimer = new CountDownTimer(startTime, 1) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long minutes = millisUntilFinished / 60000;
+                long seconds = (millisUntilFinished / 1000) % 60;
+
+                forgotPwSentBinding.tvForgotPwSentTimer.setText(String.format("(%s:%s)",
+                        decimalFormat.format(minutes), decimalFormat.format(seconds)));
+            }
+
+            @Override
+            public void onFinish() {
+                new Handler().postDelayed(() -> {
+                    forgotPwSentBinding.tvForgotPwSentResent.setVisibility(View.GONE);
+                    forgotPwSentBinding.tvForgotPwSentTimer.setVisibility(View.GONE);
+                    forgotPwSentBinding.tvForgotPwSentResend.setVisibility(View.VISIBLE);
+                    forgotPwSentBinding.btnForgotPwSentResend.setVisibility(View.VISIBLE);
+                }, 500);
+            }
+        }.start();
     }
 }
