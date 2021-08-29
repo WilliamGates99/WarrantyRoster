fd9cf7faee88844e520e07a8d63ad99d18e59e59
WilliamGates99 <william.gates.3299@gmail.com>
2021-08-29 23:47:49 +0430

    Connect Login Fragment to the API

--- app/src/main/java/com/xeniac/warrantyroster/Constants.java
+++ app/src/main/java/com/xeniac/warrantyroster/Constants.java
@@ -2,10 +2,16 @@ package com.xeniac.warrantyroster;
 
 public class Constants {
 
+    public static final String URL_GRAPHQL = "https://warranty-roster.herokuapp.com/graphql";
+
     //SharedPreference Settings Constants
     public static final String PREFERENCE_SETTINGS = "preference_settings";
     public static final String PREFERENCE_NIGHT_MODE_KEY = "night_mode";
     public static final String PREFERENCE_LANGUAGE_KEY = "language";
     public static final String PREFERENCE_COUNTRY_KEY = "country";
 
+    //SharedPreference Login Constants
+    public static final String PREFERENCE_LOGIN = "preference_login";
+    public static final String PREFERENCE_IS_LOGGED_IN_KEY = "is_logged_in";
+    public static final String PREFERENCE_USER_TOKEN_KEY = "user_token";
 }
