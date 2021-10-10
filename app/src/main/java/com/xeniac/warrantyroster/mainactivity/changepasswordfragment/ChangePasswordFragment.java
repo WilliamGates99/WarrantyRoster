f75c4530a81d268a3c8098e77c296b832c5f9d98
William Gates <william.gates.3299@gmail.com>
2021-10-10 18:09:31 +0330

    Connect Change Password Fragment to the API

--- app/src/main/java/com/xeniac/warrantyroster/mainactivity/changepasswordfragment/ChangePasswordFragment.java
+++ app/src/main/java/com/xeniac/warrantyroster/mainactivity/changepasswordfragment/ChangePasswordFragment.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
