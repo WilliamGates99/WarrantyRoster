a067998f5b705562deb74069c785e42a33927293
William Gates <william.gates.3299@gmail.com>
2021-12-24 23:54:47 +0330

    Migrate Warranty Details Fragment to Kotlin and Firebase

--- app/src/main/java/com/xeniac/warrantyrostermanager/mainactivity/MainActivity.java
+++ app/src/main/java/com/xeniac/warrantyrostermanager/mainactivity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.xeniac.warrantyrostermanager.mainactivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.shape.CornerFamily;
 import com.google.android.material.shape.MaterialShapeDrawable;
+import com.xeniac.warrantyrostermanager.Constants;
 import com.xeniac.warrantyrostermanager.LocaleModifier;
 import com.xeniac.warrantyrostermanager.R;
 import com.xeniac.warrantyrostermanager.databinding.ActivityMainBinding;
+
+import ir.tapsell.plus.AdRequestCallback;
+import ir.tapsell.plus.AdShowListener;
+import ir.tapsell.plus.TapsellPlus;
+import ir.tapsell.plus.model.TapsellPlusAdModel;
+import ir.tapsell.plus.model.TapsellPlusErrorModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,5 +77,45 @@ public class MainActivity extends AppCompatActivity {
     public void hideNavBar() {
         mainBinding.fabMain.hide();
         mainBinding.appbarMain.performHide();
+    }
+
+    public void requestInterstitialAd() {
+        TapsellPlus.requestInterstitialAd(this, Constants.DELETE_WARRANTY_Interstitial_ZONE_ID,
+                new AdRequestCallback() {
+                    @Override
+                    public void response(TapsellPlusAdModel tapsellPlusAdModel) {
+                        super.response(tapsellPlusAdModel);
+                        Log.i("requestInterstitialAd", "response: " + tapsellPlusAdModel.toString());
+                        showInterstitialAd(tapsellPlusAdModel.getResponseId());
+                    }
+
+                    @Override
+                    public void error(String s) {
+                        super.error(s);
+                        Log.e("requestInterstitialAd", "error: " + s);
+                    }
+                });
+    }
+
+    private void showInterstitialAd(String responseId) {
+        TapsellPlus.showInterstitialAd(this, responseId, new AdShowListener() {
+            @Override
+            public void onOpened(TapsellPlusAdModel tapsellPlusAdModel) {
+                super.onOpened(tapsellPlusAdModel);
+                Log.i("showInterstitialAd", "onOpened: " + tapsellPlusAdModel);
+            }
+
+            @Override
+            public void onClosed(TapsellPlusAdModel tapsellPlusAdModel) {
+                super.onClosed(tapsellPlusAdModel);
+                Log.i("showInterstitialAd", "onClosed: " + tapsellPlusAdModel);
+            }
+
+            @Override
+            public void onError(TapsellPlusErrorModel tapsellPlusErrorModel) {
+                super.onError(tapsellPlusErrorModel);
+                Log.e("showInterstitialAd", "onError: " + tapsellPlusErrorModel);
+            }
+        });
     }
 }
