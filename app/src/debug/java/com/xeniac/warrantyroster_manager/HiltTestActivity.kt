ea751b8ca24ed8fc56d965928fa8f11ac4c98f12
William Gates <william.gates.3299@gmail.com>
2022-06-08 22:34:48 +0430

    Setup Testing Files

---
+++ app/src/debug/java/com/xeniac/warrantyroster_manager/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.xeniac.warrantyroster_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
