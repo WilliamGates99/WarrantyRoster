9f45506a840dd23eb4bce71d99b2664ddfbc2d0f
William Gates <william.gates.3299@gmail.com>
2022-06-14 21:55:44 +0430

    Create Fake UserRepository

--- app/src/androidTest/java/com/xeniac/warrantyroster_manager/repositories/FakeUserRepository.kt
+++ app/src/androidTest/java/com/xeniac/warrantyroster_manager/repositories/FakeUserRepository.kt
@@ -16,16 +16,33 @@ class FakeUserRepository : UserRepository {
         if (shouldReturnNetworkError) {
             throw Exception()
         } else {
+            for (u in users) {
+                if (u.email == email) {
+                    throw Exception()
+                }
+            }
             users.add(TestUser(email, password))
         }
     }
 
     override suspend fun loginViaEmail(email: String, password: String) {
-
+        if (shouldReturnNetworkError) {
+            throw Exception()
+        } else {
+            if (!users.contains(TestUser(email, password))) {
+                throw Exception()
+            }
+        }
     }
 
     override suspend fun sendResetPasswordEmail(email: String) {
-
+        if (shouldReturnNetworkError) {
+            throw Exception()
+        } else {
+            if (users[0].email != email) {
+                throw Exception("email not found")
+            }
+        }
     }
 
     override fun getCurrentUser(): TestUser = users[0]
