ab1c0b6f713e2f9b862d7862a319a2a836da7552
William Gates <william.gates.3299@gmail.com>
2022-06-16 19:45:58 +0430

    Removed TestDataStore DI Provider

--- app/src/androidTest/java/com/xeniac/warrantyroster_manager/di/TestAppModule.kt
+++ app/src/androidTest/java/com/xeniac/warrantyroster_manager/di/TestAppModule.kt
@@ -1,29 +1,10 @@
 package com.xeniac.warrantyroster_manager.di
 
-import android.content.Context
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import javax.inject.Qualifier
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TestAppModule {
-
-    @Provides
-    @TestDataStore
-    @ExperimentalCoroutinesApi
-    fun provideTestDataStore(@ApplicationContext context: Context) =
-        PreferenceDataStoreFactory.create(
-            produceFile = { context.preferencesDataStoreFile("settings_test") }
-        )
 }
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class TestDataStore
