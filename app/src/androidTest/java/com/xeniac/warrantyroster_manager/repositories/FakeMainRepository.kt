da5c909cbab3e1a94d9b5d52011f68f21a7b3afe
William Gates <william.gates.3299@gmail.com>
2022-06-17 20:00:15 +0430

    Create FakeMainRepository

--- app/src/androidTest/java/com/xeniac/warrantyroster_manager/repositories/FakeMainRepository.kt
+++ app/src/androidTest/java/com/xeniac/warrantyroster_manager/repositories/FakeMainRepository.kt
@@ -1,44 +1,92 @@
 package com.xeniac.warrantyroster_manager.repositories
 
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.Query
-import com.xeniac.warrantyroster_manager.data.TestUser
+import com.xeniac.warrantyroster_manager.data.remote.models.Category
+import com.xeniac.warrantyroster_manager.data.remote.models.Warranty
 import com.xeniac.warrantyroster_manager.data.remote.models.WarrantyInput
 
 class FakeMainRepository : MainRepository {
 
-    private val warranties = mutableListOf<WarrantyInput>()
+    private val categories = mutableListOf<Category>()
+    private val warranties = mutableListOf<Warranty>()
+
+    private val observableCategories = MutableLiveData<List<Category>>(categories)
+    private val observableWarranties = MutableLiveData<List<Warranty>>(warranties)
 
     private var shouldReturnNetworkError = false
 
-    // TODO EDIT
     fun addWarranty(warrantyInput: WarrantyInput) {
-        warranties.add(warrantyInput)
+        val warranty = warrantyInput.let {
+            Warranty(
+                "1", it.title, it.brand, it.model, it.serialNumber, it.lifetime,
+                it.startingDate, it.expiryDate, it.description, it.categoryId
+            )
+        }
+        warranties.add(warranty)
     }
 
     fun setShouldReturnNetworkError(value: Boolean) {
         shouldReturnNetworkError = value
     }
 
+    private fun refreshLiveData() {
+        observableCategories.postValue(categories)
+        observableWarranties.postValue(warranties)
+    }
+
     override fun getCategoriesFromFirestore(): Query {
         TODO("Not yet implemented")
+//        if (shouldReturnNetworkError) {
+//            throw Exception()
+//        } else {
+//            return Firebase.firestore.collection("test")
+//        }
     }
 
     override fun getWarrantiesFromFirestore(): Query {
         TODO("Not yet implemented")
+//        if (shouldReturnNetworkError) {
+//            throw Exception()
+//        } else {
+//            return Firebase.firestore.collection("test")
+//        }
     }
 
     override suspend fun addWarrantyToFirestore(warrantyInput: WarrantyInput) {
-        TODO("Not yet implemented")
+        if (shouldReturnNetworkError) {
+            throw Exception()
+        } else {
+            addWarranty(warrantyInput)
+            refreshLiveData()
+        }
     }
 
     override suspend fun deleteWarrantyFromFirestore(warrantyId: String) {
-        TODO("Not yet implemented")
+        if (shouldReturnNetworkError) {
+            throw Exception()
+        } else {
+            for (warranty in warranties) {
+                if (warranty.id == warrantyId) {
+                    warranties.remove(warranty)
+                } else {
+                    throw Exception()
+                }
+            }
+            refreshLiveData()
+        }
     }
 
     override suspend fun updateWarrantyInFirestore(
         warrantyId: String, warrantyInput: WarrantyInput
     ) {
-        TODO("Not yet implemented")
+        if (shouldReturnNetworkError) {
+            throw Exception()
+        } else {
+            warranties.removeAt(0)
+            addWarranty(warrantyInput)
+            refreshLiveData()
+        }
     }
 
     override suspend fun getUpdatedWarrantyFromFirestore(warrantyId: String): Any {
