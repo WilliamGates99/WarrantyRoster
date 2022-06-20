7282004e30450fb29fe146d88ea1d2541c50758d
William Gates <william.gates.3299@gmail.com>
2022-06-20 21:44:26 +0430

    Setup LandingFragmentFactory

---
+++ app/src/main/java/com/xeniac/warrantyroster_manager/ui/landing/LandingFragmentFactory.kt
@@ -0,0 +1,16 @@
+package com.xeniac.warrantyroster_manager.ui.landing
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.xeniac.warrantyroster_manager.ui.landing.fragments.LoginFragment
+import com.xeniac.warrantyroster_manager.ui.landing.fragments.RegisterFragment
+import javax.inject.Inject
+
+class LandingFragmentFactory @Inject constructor() : FragmentFactory() {
+
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        return when (className) {
+            else -> super.instantiate(classLoader, className)
+        }
+    }
+}
