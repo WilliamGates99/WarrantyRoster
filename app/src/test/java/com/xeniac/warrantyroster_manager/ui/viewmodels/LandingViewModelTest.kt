0587ec171a43fe374bc76e803cc11cb266396fd0
William Gates <william.gates.3299@gmail.com>
2022-11-01 20:24:18 +0330

    Create Separate ViewModel for LandingActivity

--- app/src/test/java/com/xeniac/warrantyroster_manager/ui/viewmodels/LandingViewModelTest.kt
+++ app/src/test/java/com/xeniac/warrantyroster_manager/ui/viewmodels/LandingViewModelTest.kt
@@ -4,10 +4,6 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
 import com.xeniac.warrantyroster_manager.MainCoroutineRule
 import com.xeniac.warrantyroster_manager.data.repository.FakePreferencesRepository
-import com.xeniac.warrantyroster_manager.getOrAwaitValue
-import com.xeniac.warrantyroster_manager.utils.Constants.LOCALE_INDEX_ENGLISH_GREAT_BRITAIN
-import com.xeniac.warrantyroster_manager.utils.Constants.LOCALE_INDEX_ENGLISH_UNITED_STATES
-import com.xeniac.warrantyroster_manager.utils.Constants.LOCALE_INDEX_PERSIAN_IRAN
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
@@ -33,31 +29,5 @@ class LandingViewModelTest {
     fun isUserLoggedIn_returnsDefaultValue() {
         val isUserLoggedIn = testViewModel.isUserLoggedIn()
         assertThat(isUserLoggedIn).isFalse()
-    }
-
-    @Test
-    fun getCurrentLanguage_returnsDefaultLanguageIndex() {
-        testViewModel.getCurrentLanguage()
-
-        val responseEvent = testViewModel.currentLocaleIndexLiveData.getOrAwaitValue()
-        assertThat(responseEvent.getContentIfNotHandled()).isEqualTo(
-            LOCALE_INDEX_ENGLISH_UNITED_STATES
-        )
-    }
-
-    @Test
-    fun changeCurrentLocaleToEnglishGb_returnsFalse() {
-        testViewModel.changeCurrentLocale(LOCALE_INDEX_ENGLISH_GREAT_BRITAIN)
-
-        val responseEvent = testViewModel.changeCurrentLocaleLiveData.getOrAwaitValue()
-        assertThat(responseEvent.getContentIfNotHandled()).isFalse()
-    }
-
-    @Test
-    fun changeCurrentLocaleToPersian_returnsTrue() {
-        testViewModel.changeCurrentLocale(LOCALE_INDEX_PERSIAN_IRAN)
-
-        val responseEvent = testViewModel.changeCurrentLocaleLiveData.getOrAwaitValue()
-        assertThat(responseEvent.getContentIfNotHandled()).isTrue()
     }
 }
