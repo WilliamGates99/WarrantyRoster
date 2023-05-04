bfb3ecbef5adaa891f8656e5a20986775603c86a
William Gates <william.gates.3299@gmail.com>
2023-05-04 15:44:59 +0330

    Updated Tests for PreferencesRepository

--- app/src/androidTest/java/com/xeniac/warrantyroster_manager/core/data/repository/PreferencesRepositoryTest.kt
+++ app/src/androidTest/java/com/xeniac/warrantyroster_manager/core/data/repository/PreferencesRepositoryTest.kt
@@ -1,4 +1,4 @@
-package com.xeniac.warrantyroster_manager.data.repository
+package com.xeniac.warrantyroster_manager.core.data.repository
 
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
@@ -11,8 +11,14 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.xeniac.warrantyroster_manager.MainCoroutineRule
-import com.xeniac.warrantyroster_manager.core.data.repository.PreferencesRepositoryImpl
+import com.xeniac.warrantyroster_manager.R
 import com.xeniac.warrantyroster_manager.core.domain.repository.PreferencesRepository
+import com.xeniac.warrantyroster_manager.util.Constants.LANGUAGE_DEFAULT_OR_EMPTY
+import com.xeniac.warrantyroster_manager.util.Constants.LOCALE_INDEX_DEFAULT_OR_EMPTY
+import com.xeniac.warrantyroster_manager.util.Constants.LOCALE_INDEX_ENGLISH_GREAT_BRITAIN
+import com.xeniac.warrantyroster_manager.util.Constants.LOCALE_TAG_ENGLISH_UNITED_STATES
+import com.xeniac.warrantyroster_manager.util.Constants.THEME_INDEX_DARK
+import com.xeniac.warrantyroster_manager.util.Constants.THEME_INDEX_DEFAULT
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
@@ -54,9 +60,14 @@ class PreferencesRepositoryTest {
 
     /**
      * Fetch Initial Preferences Test Cases:
-     * getCurrentAppThemeSynchronously -> 0
-     * getIsUserLoggedInSynchronously -> false
-     * getCurrentAppTheme -> 0
+     * getCurrentAppThemeIndexSynchronously -> THEME_INDEX_DEFAULT
+     * isUserLoggedInSynchronously -> false
+     * getCategoryTitleMapKey -> LOCALE_TAG_ENGLISH_UNITED_STATES
+     * getCurrentAppLocaleIndex -> LOCALE_INDEX_DEFAULT_OR_EMPTY
+     * getCurrentAppLanguage -> LANGUAGE_DEFAULT_OR_EMPTY
+     * getCurrentAppLocaleUiText -> LOCALE_INDEX_DEFAULT_OR_EMPTY
+     * getCurrentAppThemeIndex -> THEME_INDEX_DEFAULT
+     * getCurrentAppThemeUiText -> R.string.settings_text_settings_theme_default
      * getRateAppDialogChoice -> 0
      * getPreviousRequestTimeInMillis -> 0L
      */
@@ -65,11 +76,13 @@ class PreferencesRepositoryTest {
     fun fetchInitialSynchronousPreferences() = testScope.runTest {
         testDataStore.edit { it.clear() }
 
-        val initialCurrentAppTheme = testRepository.getCurrentAppThemeIndexSynchronously()
+        val initialCurrentAppThemeIndex = testRepository.getCurrentAppThemeIndexSynchronously()
         val initialIsUserLoggedIn = testRepository.isUserLoggedInSynchronously()
+        val initialCategoryTitleMapKey = testRepository.getCategoryTitleMapKey()
 
-        assertThat(initialCurrentAppTheme).isEqualTo(0)
+        assertThat(initialCurrentAppThemeIndex).isEqualTo(THEME_INDEX_DEFAULT)
         assertThat(initialIsUserLoggedIn).isFalse()
+        assertThat(initialCategoryTitleMapKey).isEqualTo(LOCALE_TAG_ENGLISH_UNITED_STATES)
     }
 
     @Test
@@ -77,18 +90,33 @@ class PreferencesRepositoryTest {
         testDataStore.edit { it.clear() }
 
         val initialCurrentAppTheme = testRepository.getCurrentAppThemeIndex()
+        val initialCurrentAppLocaleIndex = testRepository.getCurrentAppLocaleIndex()
+        val initialCurrentAppLanguage = testRepository.getCurrentAppLanguage()
+        val initialCurrentAppLocaleUiText = testRepository.getCurrentAppLocaleUiText()
+        val initialCurrentAppThemeIndex = testRepository.getCurrentAppThemeIndex()
+        val initialCurrentAppThemeUiText = testRepository.getCurrentAppThemeUiText()
         val initialRateAppDialogChoice = testRepository.getRateAppDialogChoice()
         val initialPreviousRequestTimeInMillis = testRepository.getPreviousRequestTimeInMillis()
 
-        assertThat(initialCurrentAppTheme).isEqualTo(0)
+        assertThat(initialCurrentAppTheme).isEqualTo(LOCALE_INDEX_DEFAULT_OR_EMPTY)
+        assertThat(initialCurrentAppLocaleIndex).isEqualTo(LOCALE_INDEX_DEFAULT_OR_EMPTY)
+        assertThat(initialCurrentAppLanguage).isEqualTo(LANGUAGE_DEFAULT_OR_EMPTY)
+        assertThat(initialCurrentAppLocaleUiText.asString(context)).isEqualTo(
+            LOCALE_INDEX_DEFAULT_OR_EMPTY
+        )
+        assertThat(initialCurrentAppThemeIndex).isEqualTo(THEME_INDEX_DEFAULT)
+        assertThat(initialCurrentAppThemeUiText.asString(context)).isEqualTo(
+            R.string.settings_text_settings_theme_default
+        )
         assertThat(initialRateAppDialogChoice).isEqualTo(0)
         assertThat(initialPreviousRequestTimeInMillis).isEqualTo(0L)
     }
 
     /**
      * Write Preferences Test Cases:
-     * setIsUserLoggedIn
-     * setAppTheme
+     * isUserLoggedIn
+     * setCurrentAppLocale
+     * setCurrentAppTheme
      * setRateAppDialogChoice
      * setPreviousRequestTimeInMillis
      */
@@ -104,14 +132,25 @@ class PreferencesRepositoryTest {
     }
 
     @Test
-    fun writeAppTheme() = testScope.runTest {
+    fun writeCurrentAppLocale() = testScope.runTest {
         testDataStore.edit { it.clear() }
 
-        val testValue = 1
+        val testValue = LOCALE_INDEX_ENGLISH_GREAT_BRITAIN
+        testRepository.setCurrentAppLocale(testValue)
+
+        val currentAppLocaleIndex = testRepository.getCurrentAppLocaleIndex()
+        assertThat(currentAppLocaleIndex).isEqualTo(testValue)
+    }
+
+    @Test
+    fun writeCurrentAppTheme() = testScope.runTest {
+        testDataStore.edit { it.clear() }
+
+        val testValue = THEME_INDEX_DARK
         testRepository.setCurrentAppTheme(testValue)
 
-        val currentAppTheme = testRepository.getCurrentAppThemeIndex()
-        assertThat(currentAppTheme).isEqualTo(testValue)
+        val currentAppThemeIndex = testRepository.getCurrentAppThemeIndex()
+        assertThat(currentAppThemeIndex).isEqualTo(testValue)
     }
 
     @Test
@@ -121,8 +160,8 @@ class PreferencesRepositoryTest {
         val testValue = 1
         testRepository.setRateAppDialogChoice(testValue)
 
-        val isInAppReviewsShown = testRepository.getRateAppDialogChoice()
-        assertThat(isInAppReviewsShown).isEqualTo(testValue)
+        val rateAppDialogChoice = testRepository.getRateAppDialogChoice()
+        assertThat(rateAppDialogChoice).isEqualTo(testValue)
     }
 
     @Test
