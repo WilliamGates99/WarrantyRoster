17c52a54dfb6caa2bcda979add38efaa52094a1d
William Gates <william.gates.3299@gmail.com>
2023-05-08 18:49:36 +0330

    Updated ChangePasswordFragment Integration Tests

--- app/src/main/java/com/xeniac/warrantyroster_manager/settings/presentation/change_password/ChangePasswordFragment.kt
+++ app/src/main/java/com/xeniac/warrantyroster_manager/settings/presentation/change_password/ChangePasswordFragment.kt
@@ -8,6 +8,7 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -75,12 +76,19 @@ class ChangePasswordFragment @Inject constructor(
         subscribeToObservers()
         changePasswordOnClick()
         changePasswordActionDone()
+        requireActivity().onBackPressedDispatcher.addCallback(onBackPressedCallback)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         snackbar?.dismiss()
         _binding = null
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            navigateBack()
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
