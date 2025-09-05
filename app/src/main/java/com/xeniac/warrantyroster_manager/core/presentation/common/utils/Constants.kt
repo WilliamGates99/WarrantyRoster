2cae9e22baee0d3f6b669dbdfcb4386391e1b64f
William <william.gates.3299@gmail.com>
2025-09-05 18:53:09 +0330

    Minor improvements

--- app/src/main/java/com/xeniac/warrantyroster_manager/core/presentation/common/utils/Constants.kt
+++ app/src/main/java/com/xeniac/warrantyroster_manager/core/presentation/common/utils/Constants.kt
@@ -3,6 +3,5 @@ package com.xeniac.warrantyroster_manager.core.presentation.common.utils
 object Constants {
 
     // Web URLs
-    const val URL_DSFUT = "https://dsfut.net"
-    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/futsale/privacy_policy"
+    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/WarrantyRoster/privacy_policy"
 }
