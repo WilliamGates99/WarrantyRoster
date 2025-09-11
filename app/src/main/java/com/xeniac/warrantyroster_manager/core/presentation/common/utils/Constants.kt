23b8a42d3034d859afde7acea356aeb16690454a
William <william.gates.3299@gmail.com>
2025-09-11 13:56:37 +0330

    Updated privacy policy url

--- app/src/main/java/com/xeniac/warrantyroster_manager/core/presentation/common/utils/Constants.kt
+++ app/src/main/java/com/xeniac/warrantyroster_manager/core/presentation/common/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.xeniac.warrantyroster_manager.core.presentation.common.utils
 object Constants {
 
     // Web URLs
-    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/WarrantyRoster/privacy_policy"
+    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/warrantyroster/privacy_policy"
 }
