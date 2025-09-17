b987dc08609770f454670dde4107a53cf1d203a3
William <william.gates.3299@gmail.com>
2025-09-17 15:28:29 +0330

    Minor improvements

--- app/src/main/java/com/xeniac/warrantyroster_manager/core/presentation/common/ui/components/LoadingAnimations.kt
+++ app/src/main/java/com/xeniac/warrantyroster_manager/core/presentation/common/ui/components/LoadingAnimations.kt
@@ -10,9 +10,19 @@ import androidx.compose.animation.shrinkOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ContentLoadingAnimation(
@@ -36,5 +46,49 @@ fun ContentLoadingAnimation(
                 else -> content()
             }
         }
+    }
+}
+
+@Composable
+fun ContentLoadingAnimation(
+    isContentLoading: Boolean,
+    modifier: Modifier = Modifier,
+    enterTransition: EnterTransition = fadeIn() + expandIn(),
+    exitTransition: ExitTransition = shrinkOut() + fadeOut(),
+    content: @Composable () -> Unit
+) {
+    AnimatedContent(
+        targetState = isContentLoading,
+        transitionSpec = { enterTransition.togetherWith(exitTransition) },
+        modifier = modifier.fillMaxSize()
+    ) { isLoading ->
+        when {
+            isLoading -> LoadingAnimation()
+            else -> content()
+        }
+    }
+}
+
+@Composable
+fun LoadingAnimation(
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues(all = 32.dp),
+    indicatorColor: Color = MaterialTheme.colorScheme.primary,
+    indicatorTrackColor: Color = Color.Transparent,
+    indicatorStrokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth,
+    indicatorStrokeCap: StrokeCap = StrokeCap.Round
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
+        CircularProgressIndicator(
+            color = indicatorColor,
+            trackColor = indicatorTrackColor,
+            strokeWidth = indicatorStrokeWidth,
+            strokeCap = indicatorStrokeCap
+        )
     }
 }
