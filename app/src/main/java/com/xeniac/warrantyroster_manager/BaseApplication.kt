06d78946de716dad6c26df072acdea202f595293
William <william.gates.3299@gmail.com>
2026-02-03 17:30:42 +0330

    Enabled compose stack traces

--- app/src/main/java/com/xeniac/warrantyroster_manager/BaseApplication.kt
+++ app/src/main/java/com/xeniac/warrantyroster_manager/BaseApplication.kt
@@ -7,6 +7,8 @@ import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composer
+import androidx.compose.runtime.tooling.ComposeStackTraceMode
 import androidx.compose.ui.graphics.toArgb
 import coil3.ImageLoader
 import coil3.PlatformContext
@@ -56,6 +58,7 @@ class BaseApplication : Application(), SingletonImageLoader.Factory {
         setupTimber()
         setAppTheme()
         observeNetworkConnection()
+        enableComposeStackTraces()
         initFirebaseAppCheck()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -70,6 +73,10 @@ class BaseApplication : Application(), SingletonImageLoader.Factory {
 
     private fun observeNetworkConnection() {
         NetworkObserverHelper.observeNetworkConnection(connectivityObserver)
+    }
+
+    private fun enableComposeStackTraces() {
+        Composer.setDiagnosticStackTraceMode(mode = ComposeStackTraceMode.Auto)
     }
 
     private fun initFirebaseAppCheck() {
