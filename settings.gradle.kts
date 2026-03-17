85ddd7e6f3c11965c4d8c4ea5b3639fe5a226a9d
William <william.gates.3299@gmail.com>
2026-03-17 11:19:14 +0330

    Added Foojay Resolver Plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,14 @@ pluginManagement {
     }
 }
 
+plugins {
+    /*
+    Gradle settings plugin that automatically
+    detects, downloads, and configures the required JDKs for project
+     */
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
